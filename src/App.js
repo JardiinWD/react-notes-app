b090929af0f8633475870744ec461115e345256b
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-11-04 22:30:18 +0100

    # 3 - I have implemented the notes state managment, created the onAddNote fn linked at Add btn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react'; // Import React
+import React, { useState } from 'react'; // Import React
 import './App.css';  // Import Stylesheet
 import Sidebar from './components/Sidebar';  // Import Sidebar Comp
 import Main from './components/Main'; // Import Main Comp
-
+import uuid from 'react-uuid'; // Import univoque ID
 
 function App() {
+
+  // useState method for notes
+  const [notes, setNotes] = useState([])
+
+  // onAddNote Function
+  const onAddNote = (event) => {
+    event.preventDefault()
+    // Create a new Object
+    const newNote = {
+      id: uuid(), // Univoque ID
+      title: "Untitled Note", // Initial Title
+      body: "", // Initial Body
+      lastModified: Date.now(), // When was the last time that user modified the note
+    }
+    // Update the current state
+    setNotes([newNote, ...notes])
+  }
+
   return (
     <div className="App">
       {/* Sidebar Component */}
-      <Sidebar />
+      <Sidebar notes={notes} onAddNote={onAddNote} />
       {/* Main Component */}
       <Main />
     </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
-const Sidebar = () => {
+const Sidebar = ({ notes, onAddNote }) => {
+
+    console.log(notes);
+
     return (
         /* app-sidebar */
         <div className="app-sidebar">
@@ -9,25 +12,45 @@ const Sidebar = () => {
                 {/* Title */}
                 <h1>Notes</h1>
                 {/* Button */}
-                <button>Add</button>
+                <button onClick={onAddNote}>Add</button>
             </div>
             {/* app-sidebar-notes */}
             <div className="app-sidebar-notes">
-                {/* app-sidebar-note */}
-                <div className="app-sidebar-note">
-                    {/* sidebar-note-title */}
-                    <div className="sidebar-note-title">
-                        {/* Title */}
-                        <strong>Title</strong>
-                        {/* Delete */}
-                        <button>Delete</button>
-                    </div>
-                    <p>Note Preview</p>
-                    {/* note-meta */}
-                    <small className="note-meta">
-                        Last modified [date]
-                    </small>
-                </div>
+                {
+                    notes.map(note => {
+                        // Destructuring of Note
+                        const { id, title, body, lastModified } = note
+                        // Create the option for the dateTimeFormat
+                        let option = {
+                            year: 'numeric',
+                            month: 'numeric',
+                            day: 'numeric',
+                            hour: 'numeric',
+                            minute: 'numeric',
+                            second: 'numeric',
+                            hour12: false,
+                        }
+                        return (
+                            /* app-sidebar-note */
+                            <div key={id} className="app-sidebar-note">
+                                {/* sidebar-note-title */}
+                                <div className="sidebar-note-title">
+                                    {/* Title */}
+                                    <strong>{title}</strong>
+                                    {/* Delete */}
+                                    <button>Delete</button>
+                                </div>
+                                {/* Body with substring method */}
+                                <p>{body && body.substring(0, 100)}...</p>
+                                {/* note-meta */}
+                                <small className="note-meta">
+                                    {/* DateTimeFormat of lastModified */}
+                                    Last modified {new Intl.DateTimeFormat('it-IT', option).format(lastModified)}
+                                </small>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
